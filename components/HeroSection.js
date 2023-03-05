d6653e62913212a3be442974108f8a23441b7053
Balint Korosi <balint.j.korosi@gmail.com>
2023-03-05 07:28:16 +0100

    yolo

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -13,21 +13,21 @@ import {
 const HeroSection = () => {
   return (
     <section id="home">
-      <div className="flex flex-col text-center items-center justify-center my-10 py-12 md:pt-48 md:flex-row md:space-x-4 md:text-left">
+      <div className="flex flex-col items-center justify-center py-12 my-10 text-center md:pt-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
           <Image
             src="/balint-korosi.jpg"
             alt=""
             width={325}
             height={325}
-            className=" rounded-full shadow-2xl p-1 dark:bg-orange-300 border "
+            className="p-1 border rounded-full shadow-2xl dark:bg-orange-300"
           />
         </div>
         <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl ">
-            Hi, I&#39;m Balint!
+          <h1 className="mt-6 text-4xl font-bold md:mt-0 md:text-7xl ">
+            Hi, I&#39;m Bálint!
           </h1>
-          <p className="text-lg mt-4 mb-6 md:text-2xl">
+          <p className="mt-4 mb-6 text-lg md:text-2xl">
             I&apos;m committed to commit code for
             <span className="font-semibold text-purple-400"> 365 Days </span>
             straight on GitHub so I can expand my skills and become a proficient
@@ -36,7 +36,7 @@ const HeroSection = () => {
 
           <Link
             to="projects"
-            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700 cursor-pointer dark:bg-red-400 dark:hover:bg-orange-300"
+            className="px-6 py-3 font-semibold bg-teal-600 rounded shadow cursor-pointer text-neutral-100 hover:bg-teal-700 dark:bg-red-400 dark:hover:bg-orange-300"
             activeClass="active"
             spy={true}
             smooth={true}
@@ -56,7 +56,7 @@ const HeroSection = () => {
       <div className="flex flex-row justify-between mt-10">
         <a href="https://github.com/korbal" rel="noreferrer" target="_blank">
           <AiOutlineGithub
-            className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+            className="transition-transform cursor-pointer hover:-translate-y-1 text-neutral-500 dark:text-neutral-100"
             size={90}
           />
         </a>
@@ -67,7 +67,7 @@ const HeroSection = () => {
           target="_blank"
         >
           <AiOutlineMail
-            className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+            className="transition-transform cursor-pointer hover:-translate-y-1 text-neutral-500 dark:text-neutral-100"
             size={100}
           />
         </a>
@@ -78,13 +78,13 @@ const HeroSection = () => {
           target="_blank"
         >
           <AiOutlineTwitter
-            className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+            className="transition-transform cursor-pointer hover:-translate-y-1 text-neutral-500 dark:text-neutral-100"
             size={90}
           />
         </a>
       </div>
 
-      <div className="flex flex-row items-center text-center justify-center mt-5">
+      <div className="flex flex-row items-center justify-center mt-5 text-center">
         <Link
           className="cursor-pointer"
           to="about"
